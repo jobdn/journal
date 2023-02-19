2e323b91a90652e56db34b437e9f485e04feb65a
Pisarev Danila <pisarev@softlab.ru>
2023-02-19 21:35:03 +0300

    feat: check loki ci working

--- src/pages/home/ui/Home.tsx
+++ src/pages/home/ui/Home.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
   const { t } = useTranslation("home");
-  return <h1>{t("title")}</h1>;
+  return <h1 style={{ background: "red" }}>{t("title")}</h1>;
 };
 
 export default HomePage;
